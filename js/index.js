7b8655a3d0669b9f2b8ded45028e03905c7f7e51
George Clapham Holguín <claphamholguingeorge@gmail.com>
2024-12-20 20:30:00 -0500

    mejora de aspectos visuales (CSS)

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+
+var offcanvasNavLinks = document.getElementsByClassName('offcanvas-nav-link');
+var myOffcanvas = document.getElementById('offcanvasNavbar');
+var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+var offcanvasBackdrop = null;
+
+for (let index = 0; index < offcanvasNavLinks.length; index++) {
+    offcanvasNavLinks[index].addEventListener('click', function () {
+        bsOffcanvas.hide();
+    })
+}
+
+
