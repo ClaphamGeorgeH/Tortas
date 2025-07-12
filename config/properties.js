7cea15642353008bcf1e772fd425c3ee9cdc84b2
George Clapham Holguín <claphamholguingeorge@gmail.com>
2025-07-11 23:28:15 -0500

    project finished

---
+++ config/properties.js
@@ -0,0 +1,1 @@
+export const cellphoneNumber ='51985804828';
--- data/tortas.js
+++ data/tortas.js
@@ -1,11 +1,15 @@
+const cellphoneNumber = 51985804828; 
+
 const cakes = [
     {
         "name" : "Pie de limón",
         "price" : 55,
         "currency" : "S/",
+        "hasHalf" : false,
         "price_half": 0,
-        "path" : "images/pie_de_limon_2.jpg",
-        "active?" : true,
+        "path" : "assets/images/pie_de_limon_2.jpg",
+        "isActive" : true,
+        "isWeighed" : false,
         "amount" : null,
         "amount_measure_unit": null,
     },
@@ -13,9 +17,11 @@ const cakes = [
         "name" : "Torta galés de frutos secos",
         "price" : 50,
         "currency" : "S/",
+        "hasHalf" : true,
         "price_half" : 27.50,
-        "path" : "images/torta_galesa_de_frutos_secos.jpeg",
-        "active?" : true,
+        "path" : "assets/images/torta_galesa_de_frutos_secos.jpeg",
+        "isActive" : true,
+        "isWeighed" : true,
         "amount" : 1800,
         "amount_measure_unit": 'gr',
     },
@@ -23,9 +29,11 @@ const cakes = [
         "name" : "Turrón bitter de chocolate y frutos secos",
         "price" : 80,
         "currency" : "S/",
+        "hasHalf" : true,
         "price_half": 42.50,
-        "path" : "images/turron_bitter_de_chocolate.jpeg",
-        "active?" : true,
+        "path" : "assets/images/turron_bitter_de_chocolate.jpeg",
+        "isActive" : true,
+        "isWeighed" : true,
         "amount" : 1700,
         "amount_measure_unit": 'gr',
     },
@@ -33,9 +41,11 @@ const cakes = [
         "name" : "Turrón de pecanas",
         "price" : 75,
         "currency" : "S/",
+        "hasHalf" : true,
         "price_half": 40,
-        "path" : "images/turron_de_pecanas.jpeg",
-        "active?" : true,
+        "path" : "assets/images/turron_de_pecanas.jpeg",
+        "isActive" : true,
+        "isWeighed" : true,
         "amount" : 1700,
         "amount_measure_unit": 'gr',
     }
@@ -43,36 +53,61 @@ const cakes = [
 
 const cakes_list = document.getElementById("dessert-list-container");
 
+
 cakes.forEach( cake => {
 
-    if (!cake["active?"]) return;
+    if (!cake["isActive"]) return;
 
-    const cakeDiv = document.createElement("div");
-    cakeDiv.className = "col-12 col-md-4 align-self-center dessert-card me-4";
-    cakeDiv.innerHTML = 
+    var cakeDiv = document.createElement("div");
+    cakeDiv.className = "dessert-card col-12 col-md-4";
+    
+    /*cakeDiv.innerHTML = 
             `<a href="">
                 <img src=${cake.path} class="img-fluid" alt="...">
             </a>
             <div class="dessert-card-body">
                 <div class="row">
                     <div class="col-10">
-                        <a href="https://wa.me/51985804828?text=I'm%20interested%20in%20your%20car%20for%20sale">
-                            <p class="dessert-card-body-description" style="text-align:left; margin:0px; font-weight: bold;">${cake.name}</p>
+                        <a href="https://wa.me/ Estoy interesado en la torta ${cake.name} de precio ${cake.currency}${cake.price} / ${cake.amount}${cake.amount_measure_unit}. ¿Me daria información al respecto? Gracias.">
+                            <p class="dessert-card-body-description" style="text-align:left; margin:0px; font-weight: bold;">${cake.name + " ("+cake.amount+cake.amount_measure_unit+")" }</p>
                         </a>
                     </div>
                     <div class="col-2">
-                        <p class="dessert-card-body-price" style="text-align:right; margin:0px">${cake.currency}${cake.price}</p>
+                        <p class="dessert-card-body-price text-end fw-bolder">${cake.currency}${cake.price}</p>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-6">
-                        <p class="dessert-card-body-description" style="text-align:left; margin:0px">${cake.amount}${cake.amount_measure_unit}</p>
-                    </div>
-                    <div class="col-6">
-                        <p class="dessert-card-body-price" style="text-align:right; margin:0px">precio mitad: ${cake.currency}${cake.price_half}</p>
+                    <div class="col-12">
+                        <p class="dessert-card-body-price text-end fw-bolder">${cake.hasHalf ? "precio mitad: "+cake.currency+cake.price_half : ""}</p>
                     </div>
                 </div>
-            </div>`;           
+            </div>`;    */
+           
+    cakeDiv.innerHTML =
+        `
+            <div class="card">
+                <a href="https://wa.me/${cellphoneNumber}?text=Estoy interesado en la torta ${cake.name} . ¿Me más daria información al respecto? Gracias.">
+                    <img src="${cake.path}" class="card-img-top" alt="...">
+                </a>
+                <div class="card-body">
+                    <div class="row">
+                    <div class="col-9">
+                        <a href="https://wa.me/${cellphoneNumber}?text=Estoy interesado en la torta ${cake.name}. ¿Me daria más información al respecto? Gracias.">
+                            <p class="dessert-card-body-description" style="text-align:left; margin:0px; font-weight: bold;">${cake.name}${cake.isWeighed ? " ("+cake.amount+cake.amount_measure_unit+")":""}</p>
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <p class="dessert-card-body-price text-end fw-bolder">${cake.currency}${cake.price}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p class="dessert-card-body-price text-end fw-bolder">${cake.hasHalf ? "precio mitad: "+cake.currency+cake.price_half : ""}</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        `;
     cakes_list.appendChild(cakeDiv);
     
 });
